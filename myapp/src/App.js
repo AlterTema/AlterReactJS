30ba3784a8939a0ee28d11311a674ca7519a9225
AlterTema <altertema@gmail.com>
2021-09-06 20:58:07 +0300

    Initial commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
+import {SimpleText} from './components';
 
-function App(props) {
+
+function App() {
+//...
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="generalText">Привет, меня зовут Артем</div>
-        <h3 class="centerText">И это мое первое приложение на курсе ReactJS</h3>
-        <h2 ><a class="endingText" href="https://github.com/AlterTema">{props.name}</a></h2>
-      </header>
-    </div >
+    <div className = "App">
+      <SimpleText name="AlterTema"/>
+    </div>
   );
 }
 

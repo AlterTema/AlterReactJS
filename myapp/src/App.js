14ebd189b4df04bbb15b8946c9f76b2891f57f46
AlterTema <altertema@gmail.com>
2021-09-09 21:21:48 +0300

    Initial commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import {SimpleText} from './components';
+import {MessageList} from '../src/components/MessageList'
+
+
 
 
 function App() {
 //...
+   
+    
   return (
     <div className = "App">
       <SimpleText name="AlterTema"/>
+   <MessageList />
+    
     </div>
   );
 }
